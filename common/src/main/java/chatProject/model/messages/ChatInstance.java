fdc6dcfb60847977cb0e200896f6270506ab2da1
Flynt-hub <cerna.david.38@gmail.com>
2021-11-16 10:58:38 +0100

    [FIXED] ChatInstance have now functionnal list and hashmap

--- common/src/main/java/chatProject/model/messages/ChatInstance.java
+++ common/src/main/java/chatProject/model/messages/ChatInstance.java
@@ -92,7 +92,8 @@ public class ChatInstance<T> {
      * @return the new chat instance
      */
     public static <T> ChatInstance<T> initEmptyChat() {
-        return new ChatInstance<T>(null, null);
+//        return new ChatInstance<T>(null, null);
+        return new ChatInstance<T>( new ArrayList<Chatroom<T>>(),  new HashMap<UserInfo, LocalTime>());
     }
 
 }
