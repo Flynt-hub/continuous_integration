770e10acd991e706d17ae5e2f7a708d1792d00be
Kuro38 <isshia.inapogui@epsi.fr>
2021-11-29 16:47:39 +0100

    Ajout test AddMessage() dans commmon

--- common/src/test/java/chatProject/model/messages/ChatroomTest.java
+++ common/src/test/java/chatProject/model/messages/ChatroomTest.java
@@ -5,6 +5,8 @@ import chatProject.model.user.UserAccount;
 import chatProject.model.user.UserInfo;
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class ChatroomTest {
@@ -37,14 +39,20 @@ public class ChatroomTest {
         //Assert
         assertEquals("Le owner doit être le même",owner, chatroom.getOwner());
     }
-    /*
+
     @Test
     public void addMessage(){
 
         //Arrange
+        String content = "ceci est un message";
+        ArrayList<Message<String>> messages= new ArrayList<Message<String>>() {};
+        final Message<String> message = new Message<String>(0, null, content);
 
         //Act
+        messages.add(message);
+        Chatroom<String> chatroom = new Chatroom<String>(null,null, messages);
 
         //Assert
-    } */
+        assertEquals(chatroom.addMessage(null, content), messages.get(1));
+    }
 }
