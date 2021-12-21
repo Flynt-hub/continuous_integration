5b93ed6b11543b473d680e568fc4ce1e6583473d
Kuro38 <isshia.inapogui@epsi.fr>
2021-12-21 13:25:12 +0100

    Ajout addUser_2 dans ChatInstanceTest

--- common/src/test/java/chatProject/model/messages/ChatInstanceTest.java
+++ common/src/test/java/chatProject/model/messages/ChatInstanceTest.java
@@ -68,6 +68,54 @@ public class ChatInstanceTest<T> {
     }
 
     @Test
+    public void addUser_1(){
+
+        //Arrange
+        ArrayList<Chatroom<T>> chatrooms = new ArrayList<Chatroom<T>>() {};
+
+        UserAccount account = new UserAccount(1, "MyUser");
+        Status status = Status.ACTIVE;
+        UserInfo userInfo = new UserInfo(account, status);
+
+        Map<UserInfo, LocalTime> map = new HashMap<>();
+
+
+        //Act
+        map.put(userInfo, LocalTime.now());
+        ChatInstance<T> chatInstance = new ChatInstance<T>(chatrooms, map);
+
+        //Assert
+        assertFalse("le nouvel utilisateur à été ajouté, retourne false", chatInstance.addUser(userInfo));
+    }
+
+    @Test
+    public void addUser_2(){
+
+        //Arrange
+        ArrayList<Chatroom<T>> chatrooms = new ArrayList<Chatroom<T>>() {};
+
+        UserAccount account_1 = new UserAccount(1, "MyUser_1");
+        UserAccount account_2 = new UserAccount(2, "MyUser_2");
+
+        Status status = Status.ACTIVE;
+
+        UserInfo userInfo_1 = new UserInfo(account_1, status);
+        UserInfo userInfo_2 = new UserInfo(account_2, status);
+        Map<UserInfo, LocalTime> map = new HashMap<>();
+
+
+        //Act
+        map.put(userInfo_1, LocalTime.now());
+
+        ChatInstance<T> chatInstance = new ChatInstance<T>(chatrooms, map);
+
+        //Assert
+        assertTrue("le nouvel utilisateur existe pas, on ajoute l'ajoute, retourne true", chatInstance.addUser(userInfo_2));
+
+    }
+
+
+    @Test
     public void getUsers(){
 
         //Arrange
